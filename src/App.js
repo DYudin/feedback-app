d683af11a0daa1009182b9f3a73e840d7ae44001
DYudin <extreemdv@mail.ru>
2022-01-08 01:52:10 +0500

    remove unused usings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 
-import { useState } from "react"
-import { BrowserRouter as Router, Route, Routes, NavLink} from 'react-router-dom'
-import Card from './components/shared/Card'
+
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from './pages/AboutPage';
@@ -13,7 +11,7 @@ import AboutIconLink from './components/AboutIconLink'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-  
+
   return (   
     <FeedbackProvider>
       <Router>
